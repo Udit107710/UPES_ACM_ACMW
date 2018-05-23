16a5eb4ab95b1ff54da8da95ce33026c264c289b
Ansh <500061120@stu.upes.ac.in>
2018-05-23 17:09:25 +0530

    Structure for Login profile is added

---
+++ app/src/main/java/org/upesacm/acmacmw/LoginProfile.java
@@ -0,0 +1,13 @@
+package org.upesacm.acmacmw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_profile);
+    }
+}
