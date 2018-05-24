afec3f66fd6a983d5dedbc6a59b25cce1959f685
Ansh <500061120@stu.upes.ac.in>
2018-05-24 17:03:35 +0530

    Event Registration layout

--- app/src/main/java/org/upesacm/acmacmw/EventRegistration.java
+++ app/src/main/java/org/upesacm/acmacmw/EventRegistration.java
@@ -7,7 +7,9 @@ public class EventRegistration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_registration);
+
     }
 }
