59fd32cf2424dbe86441c406819e00cc076b8fdf
shivam1196 <sumancrown101@gmail.com>
2018-06-15 11:08:16 +0530

    few ui changes

--- app/src/main/java/org/upesacm/acmacmw/util/RandomOTPGenerator.java
+++ app/src/main/java/org/upesacm/acmacmw/util/RandomOTPGenerator.java
@@ -8,8 +8,8 @@ public class RandomOTPGenerator {
     public static String generate(int seed,int length) {
         Random random=new Random(seed+ System.nanoTime());
 
-        String capitalAlphabets="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String smallAlphabets="abcdefghijklmnopqrstuvwxyz";
+        String capitalAlphabets="012345789";
+        String smallAlphabets="0123456789";
         String digits="0123456789";
 
         String values=capitalAlphabets+smallAlphabets+digits;
