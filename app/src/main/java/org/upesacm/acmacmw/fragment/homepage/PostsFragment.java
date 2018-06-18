6ba82cd5ff42db9368d830d65381c3faf13f6dd7
shivam1196 <sumancrown101@gmail.com>
2018-06-19 02:20:15 +0530

    Uploading image functionality improved

--- app/src/main/java/org/upesacm/acmacmw/fragment/homepage/PostsFragment.java
+++ app/src/main/java/org/upesacm/acmacmw/fragment/homepage/PostsFragment.java
@@ -8,10 +8,18 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -52,15 +60,19 @@ import org.upesacm.acmacmw.model.Member;
 import org.upesacm.acmacmw.model.Post;
 import org.upesacm.acmacmw.model.TrialMember;
 import org.upesacm.acmacmw.retrofit.HomePageClient;
+import org.upesacm.acmacmw.util.Config;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -68,6 +80,8 @@ import retrofit2.Response;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
 
 public class PostsFragment extends Fragment
@@ -95,6 +109,7 @@ public class PostsFragment extends Fragment
     Member signedInMember;
     TrialMember trialMember;
     private Uri imageUri;
+    private Uri fileUri;
 
     public PostsFragment() {
         // Required empty public constructor
@@ -117,6 +132,8 @@ public class PostsFragment extends Fragment
         postsReference= database
                 .getReference("posts/"+"Y"+calendar.get(Calendar.YEAR)+"/"
                         +"M"+calendar.get(Calendar.MONTH));
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
     }
 
 
@@ -147,7 +164,6 @@ public class PostsFragment extends Fragment
 
         return view;
     }
-
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.cameraButton) {
@@ -184,10 +200,47 @@ public class PostsFragment extends Fragment
 
 
     /* ************************** functions for taking picture ***********************************/
+    public Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+    private static File getOutputMediaFile(int type) {
+
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                Config.IMAGE_DIRECTORY_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("fail", "Oops! Failed create "
+                        + Config.IMAGE_DIRECTORY_NAME + " directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "IMG_" + timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
     private void dispatchTakePictureIntent() {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-//                File imageFile = null;
+            try {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+                    //                File imageFile = null;
 //                try {
 //                    imageFile = createImageFile();
 //                }catch(IOException ioe) {
@@ -199,7 +252,13 @@ public class PostsFragment extends Fragment
 //                    System.out.println("imageUri : "+imageUri);
 //                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
                     startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-               // }
+                    // }
+                }
+            }
+            catch (OutOfMemoryError error)
+            {
+                Toast.makeText(getContext(), "Unable to Capture image because"+error.getCause(), Toast.LENGTH_SHORT).show();
+
             }
     }
 
@@ -226,11 +285,40 @@ public class PostsFragment extends Fragment
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         System.out.println("onActivityResult Called");
         Bitmap imageBitmap = null;
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             System.out.println("request image capture");
+                if(fileUri!=null)
+                {
+                    try {
 
+                        ByteArrayOutputStream os = new ByteArrayOutputStream();
+                        BitmapFactory.Options options = new BitmapFactory.Options();
+//                        int nh = (int) ( bitmap.getHeight() * (512.0 / bitmap.getWidth()) );
+//                        Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 512, nh, true);
+                        String uri=compressImage(fileUri.toString());
+                        fileUri= Uri.parse(uri);
+                        Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(), options);
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, os);
+                        byte[] byteArray = os.toByteArray();
+                        System.out.println("byte array : " + byteArray);
+                        for (int i = 0; i < byteArray.length; i++) {
+                            System.out.print(byteArray[i]);
+                        }
+                        Bundle args = new Bundle();
+                        args.putByteArray("image_data", byteArray);
+                        this.onNewPostDataAvailable(args);
+                    }
+                    catch (OutOfMemoryError error)
+                    {
+                        Log.d("out of memory",""+error.getCause());
+                        Toast.makeText(getContext(),"unable to upload image:"+error.getCause(),Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
 //            getActivity().getContentResolver().notifyChange(imageUri, null);
 //            ContentResolver cr = getActivity().getContentResolver();
 //            Bitmap bitmap;
@@ -244,18 +332,17 @@ public class PostsFragment extends Fragment
 //                Toast.makeText(getContext(), "Failed to load", Toast.LENGTH_SHORT).show();
 //                Log.d("tag", "Failed to load", e);
 //            }
+
+
+            /*
             Bundle extras = data.getExtras();
             imageBitmap = (Bitmap) extras.get("data");
             ByteArrayOutputStream stream = new ByteArrayOutputStream();
             imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
             byte[] byteArray = stream.toByteArray();
-            System.out.println("byte array : "+byteArray);
-            for(int i=0;i<byteArray.length;i++) {
-                System.out.print(byteArray[i]);
-            }
-            Bundle args=new Bundle();
-            args.putByteArray("image_data",byteArray);
-            this.onNewPostDataAvailable(args);
+             */
+
+
 
         } else if (requestCode == CHOOSE_FROM_GALLERY && resultCode == RESULT_OK && resultCode!=RESULT_CANCELED) {
             System.out.println("choose from gallery");
@@ -263,7 +350,9 @@ public class PostsFragment extends Fragment
             try {
                 imageBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                int nh = (int) ( imageBitmap.getHeight() * (1024.0 / imageBitmap.getWidth()) );
+                Bitmap scaled = Bitmap.createScaledBitmap(imageBitmap, 1024, nh, true);
+                scaled.compress(Bitmap.CompressFormat.PNG, 100, stream);
                 byte[] byteArray = stream.toByteArray();
                 Bundle args=new Bundle();
                 args.putByteArray("image_data",byteArray);
@@ -447,6 +536,163 @@ public class PostsFragment extends Fragment
         this.trialMember=null;
         recyclerViewAdapter.setTrialMember(null);
     }
+    public String compressImage(String imageUri) {
 
+        String filePath = getRealPathFromURI(imageUri);
+        Bitmap scaledBitmap = null;
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+//      by setting this field as true, the actual bitmap pixels are not loaded in the memory. Just the bounds are loaded. If
+//      you try the use the bitmap here, you will get null.
+        options.inJustDecodeBounds = true;
+        Bitmap bmp = BitmapFactory.decodeFile(filePath, options);
+
+        int actualHeight = options.outHeight;
+        int actualWidth = options.outWidth;
+
+//      max Height and width values of the compressed image is taken as 816x612
+
+        float maxHeight = 816.0f;
+        float maxWidth = 612.0f;
+        float imgRatio = actualWidth / actualHeight;
+        float maxRatio = maxWidth / maxHeight;
+
+//      width and height values are set maintaining the aspect ratio of the image
+
+        if (actualHeight > maxHeight || actualWidth > maxWidth) {
+            if (imgRatio < maxRatio) {
+                imgRatio = maxHeight / actualHeight;
+                actualWidth = (int) (imgRatio * actualWidth);
+                actualHeight = (int) maxHeight;
+            } else if (imgRatio > maxRatio) {
+                imgRatio = maxWidth / actualWidth;
+                actualHeight = (int) (imgRatio * actualHeight);
+                actualWidth = (int) maxWidth;
+            } else {
+                actualHeight = (int) maxHeight;
+                actualWidth = (int) maxWidth;
+
+            }
+        }
+
+//      setting inSampleSize value allows to load a scaled down version of the original image
+
+        options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight);
+
+//      inJustDecodeBounds set to false to load the actual bitmap
+        options.inJustDecodeBounds = false;
+
+//      this options allow android to claim the bitmap memory if it runs low on memory
+        options.inPurgeable = true;
+        options.inInputShareable = true;
+        options.inTempStorage = new byte[16 * 1024];
+
+        try {
+//          load the bitmap from its path
+            bmp = BitmapFactory.decodeFile(filePath, options);
+        } catch (OutOfMemoryError exception) {
+            exception.printStackTrace();
+
+        }
+        try {
+            scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888);
+        } catch (OutOfMemoryError exception) {
+            exception.printStackTrace();
+        }
+
+        float ratioX = actualWidth / (float) options.outWidth;
+        float ratioY = actualHeight / (float) options.outHeight;
+        float middleX = actualWidth / 2.0f;
+        float middleY = actualHeight / 2.0f;
+
+        Matrix scaleMatrix = new Matrix();
+        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
+
+        Canvas canvas = new Canvas(scaledBitmap);
+        canvas.setMatrix(scaleMatrix);
+        canvas.drawBitmap(bmp, middleX - bmp.getWidth() / 2, middleY - bmp.getHeight() / 2, new Paint(Paint.FILTER_BITMAP_FLAG));
+
+//      check the rotation of the image and display it properly
+        ExifInterface exif;
+        try {
+            exif = new ExifInterface(filePath);
+
+            int orientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION, 0);
+            Log.d("EXIF", "Exif: " + orientation);
+            Matrix matrix = new Matrix();
+            if (orientation == 6) {
+                matrix.postRotate(90);
+                Log.d("EXIF", "Exif: " + orientation);
+            } else if (orientation == 3) {
+                matrix.postRotate(180);
+                Log.d("EXIF", "Exif: " + orientation);
+            } else if (orientation == 8) {
+                matrix.postRotate(270);
+                Log.d("EXIF", "Exif: " + orientation);
+            }
+            scaledBitmap=Bitmap.createBitmap(scaledBitmap,0,0,scaledBitmap.getWidth(),scaledBitmap.getHeight());
+//
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileOutputStream out = null;
+        String filename = getFilename();
+        try {
+            out = new FileOutputStream(filename);
+
+//          write the compressed bitmap at the destination specified by filename.
+            scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 80, out);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return filename;
+
+    }
+
+    public String getFilename() {
+        File file = new File(Environment.getExternalStorageDirectory().getPath(), "MyFolder/Images");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        String uriSting = (file.getAbsolutePath() + "/" + System.currentTimeMillis() + ".jpg");
+        return uriSting;
+
+    }
+
+    private String getRealPathFromURI(String contentURI) {
+        Uri contentUri = Uri.parse(contentURI);
+        Cursor cursor = getContext().getContentResolver().query(contentUri, null, null, null, null);
+        if (cursor == null) {
+            return contentUri.getPath();
+        } else {
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(index);
+        }
+    }
+
+    public int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        final float totalPixels = width * height;
+        final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+        while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+            inSampleSize++;
+        }
+
+        return inSampleSize;
+    }
 
 }
